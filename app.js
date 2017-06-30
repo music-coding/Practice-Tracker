0027ef2ae164547c83146bcf35c4f93bac28673b
Code <code@Josiahs-MacBook-Pro.local>
2017-06-30 11:26:59 -0500

    changed port

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use('/', routes);
 
 
 // listen on port 3000
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Express app listening on port 3000');
 });
